2f7765f7065716f8c4a487630aaa1fb03ba21113
Andrea <Andrea@dell-andrea.homenet.telecomitalia.it>
2019-01-07 23:10:58 +0100

    Restore login servet

--- src/main/java/it/starwars/servlet/session/LoginServlet.java
+++ src/main/java/it/starwars/servlet/session/LoginServlet.java
@@ -6,13 +6,15 @@
 package it.starwars.servlet.session;
 
 import java.io.IOException;
-import java.util.List;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import it.starwars.bean.Utente;
 import it.starwars.service.UtenteService;
@@ -40,69 +42,58 @@ public class LoginServlet extends HttpServlet {
 		String username = MyUtils.getSafeString(request.getParameter(MyConstants.USERNAME));
 		String password = MyUtils.getSafeString(request.getParameter(MyConstants.PASSWORD));
 
-
-
+		Utente utente = null;
 		try {
-			List<Utente> list = UtenteService.getUtenti();
-			getServletContext().log("" + list.size());
+			utente = UtenteService.getUtenteById(username);
 		} catch (Exception e) {
 			getServletContext().log("Impossibile ricercare l'utente sul db", e);
 			return;
 		}
 
+		if (utente == null) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Username non corretta.</font>");
+			rd.include(request, response);
+			return;
+		}
 
-		//		Utente utente = null;
-		//		try {
-		//			utente = UtenteService.getUtenteById(username);
-		//		} catch (Exception e) {
-		//			getServletContext().log("Impossibile ricercare l'utente sul db", e);
-		//			return;
-		//		}
-		//
-		//		if (utente == null) {
-		//			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
-		//			PrintWriter out = response.getWriter();
-		//			out.println("<font color=red>Username non corretta.</font>");
-		//			rd.include(request, response);
-		//			return;
-		//		}
-		//
-		//		if (!password.equals(utente.getPassword())) {
-		//			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
-		//			PrintWriter out = response.getWriter();
-		//			out.println("<font color=red>Password non corretta.</font>");
-		//			rd.include(request, response);
-		//			return;
-		//		}
-		//
-		//		if (!utente.getAttivo()) {
-		//			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
-		//			PrintWriter out = response.getWriter();
-		//			out.println("<font color=red>Utente non attivo.</font>");
-		//			rd.include(request, response);
-		//			return;
-		//		}
-		//
-		//		// get the old session and invalidate
-		//		HttpSession oldSession = request.getSession(false);
-		//		if (oldSession != null) {
-		//			oldSession.invalidate();
-		//		}
-		//		// generate a new session
-		//		HttpSession newSession = request.getSession(true);
-		//
-		//		// setting session to expiry in 5 mins
-		//		newSession.setMaxInactiveInterval(MAX_INACTIVE_INTERVAL);
-		//
-		//		newSession.setAttribute(MyConstants.USER, utente);
-		//
-		//		try {
-		//			UtenteService.updateDateLastUpdate(utente);
-		//		} catch (Exception e) {
-		//			getServletContext().log("Impossibile aggiornare la data ultimo accesso", e);
-		//		}
-		//
-		//		response.sendRedirect(HOMEPAGE_PATH);
+		if (!password.equals(utente.getPassword())) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Password non corretta.</font>");
+			rd.include(request, response);
+			return;
+		}
+
+		if (!utente.getAttivo()) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(LOGIN_PATH);
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Utente non attivo.</font>");
+			rd.include(request, response);
+			return;
+		}
+
+		// get the old session and invalidate
+		HttpSession oldSession = request.getSession(false);
+		if (oldSession != null) {
+			oldSession.invalidate();
+		}
+		// generate a new session
+		HttpSession newSession = request.getSession(true);
+
+		// setting session to expiry in 5 mins
+		newSession.setMaxInactiveInterval(MAX_INACTIVE_INTERVAL);
+
+		newSession.setAttribute(MyConstants.USER, utente);
+
+		try {
+			UtenteService.updateDateLastUpdate(utente);
+		} catch (Exception e) {
+			getServletContext().log("Impossibile aggiornare la data ultimo accesso", e);
+		}
+
+		response.sendRedirect(HOMEPAGE_PATH);
 
 	}
 }
