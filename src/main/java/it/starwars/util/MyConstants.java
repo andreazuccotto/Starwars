9893e69540a33f58714dced5b314000eb2d2891e
andrea.zuccotto <andrea.zuccotto@accenture.com>
2019-01-25 19:33:22 +0100

    Gestione errori

--- src/main/java/it/starwars/util/MyConstants.java
+++ src/main/java/it/starwars/util/MyConstants.java
@@ -8,6 +8,5 @@ public class MyConstants {
 	public static final String MAIL_FROM = "it.starwars";
 	public static final String USER = "user";
 	public static final String HOMEPAGE_PATH = "/Starwars/main/homepage.jsp";
-	public static final String LOGIN_PATH = "/Starwars/login/login.jsp";
 
 }
